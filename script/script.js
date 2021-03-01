9888572d42b1701544bc99df6b69c9ba96df9455
Andrey Aryamnov <shon9000000@yandex.ru>
2021-03-02 00:08:54 +0700

    Внести правки ревьюера №1

--- script/script.js
+++ script/script.js
@@ -1,36 +1,33 @@
-let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input-name');
-let jobInput = formElement.querySelector('.popup__input-signature');
-let nameProfile = document.querySelector('.profile__title');
+let formElement = document.querySelector('.popup__form'); // Находим форму в DOM
+let nameInput = formElement.querySelector('.popup__input_type_name'); // Находим поля формы в DOM
+let jobInput = formElement.querySelector('.popup__input_type_signature'); 
+let nameProfile = document.querySelector('.profile__title'); // Находим значения строк профиля в DOM
 let signatureProfile = document.querySelector('.profile__subtitle');
+let popup = document.querySelector('.popup'); 
+let edditProfile = document.querySelector('.profile__button-edit'); //Находим кнопку "редактировать"
+let closeProfile = document.querySelector('.popup__close'); //Находим кнопку "закрыть"
 
-function tranferToForm () {
+function tranferToForm () { // Функция передает текст с сайта в форму
     nameInput.value = nameProfile.textContent;
     jobInput.value = signatureProfile.textContent;
 }
 
 function openPopup () {
-    let popup = document.querySelector('.popup');
     popup.classList.add('popup_opened');
     tranferToForm ();
 }
 
 function closePopup () {
-    let popup = document.querySelector('.popup');
     popup.classList.remove('popup_opened');
 }
 
-let edditProfile = document.querySelector('.profile__button-edit');
-let closeProfile = document.querySelector('.popup__close');
-
-edditProfile.addEventListener('click', openPopup);
-closeProfile.addEventListener('click', closePopup);
-
-function formSubmitHandler (evt) {
-    evt.preventDefault();
+function formSubmitHandler (evt) { 
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     nameProfile.textContent = nameInput.value;
     signatureProfile.textContent = jobInput.value;
     closePopup ();
 }
 
+edditProfile.addEventListener('click', openPopup);
+closeProfile.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
